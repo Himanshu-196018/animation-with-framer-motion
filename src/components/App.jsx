552810b0150962cfe5ccc2d5381aec1e7bbfd14f
Himanshu <himanshu_196018@saitm.org>
2023-10-25 20:51:39 +0530

    FM example 2 and navbar animation added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import Navbar from "./Navbar";
+
 const App = () => {
   return (
     <>
@@ -10,6 +12,26 @@ const App = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 3, repeat: Infinity }}
         />
+      </div>
+      <div className="wrap">
+        <motion.div
+          className="example-2"
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 180, 180, 0],
+            borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1,
+          }}
+        />
+      </div>
+      <div className="wrap">
+        <Navbar />
       </div>
     </>
   );
